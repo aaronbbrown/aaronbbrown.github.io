8821a9187883da3a29cb93c4a2616843dbe0c8ef
Aaron Brown <aaron@9minutesnooze.com>
2017-01-06 19:46:31 -0500

    upgrade gems and fix syntax errors

--- run.sh
+++ run.sh
@@ -3,20 +3,23 @@
 set -e
 set -x
 
-LOCALPORT=8080
-B2DIP=$(boot2docker ip 2>/dev/null)
-#B2DURL="http://${B2DIP}:2376"
+LOCALPORT=9080
 CONTAINERNAME="temp_9ms_blog"
 
 source build.sh
 
 # check if the container is running
-docker ps | awk '$NF=="'$CONTAINERNAME'" {print $NF}' | grep -q $CONTAINERNAME && docker stop "$CONTAINERNAME"
+docker ps | \
+  awk '$NF=="'$CONTAINERNAME'" {print $NF}' | \
+  grep -q $CONTAINERNAME && \
+  docker stop "$CONTAINERNAME"
 
 # check if the container exists
-docker ps -a | awk '$NF=="'$CONTAINERNAME'" {print $NF}' | grep -q $CONTAINERNAME && docker rm -f "$CONTAINERNAME" 
+docker ps -a | \
+  awk '$NF=="'$CONTAINERNAME'" {print $NF}' | \
+  grep -q $CONTAINERNAME && \
+  docker rm -f "$CONTAINERNAME"
 
 docker run --name "$CONTAINERNAME" -p ${LOCALPORT}:80 -d aaronbbrown/blog
 
-BLOG_URL="http://${B2DIP}:${LOCALPORT}"
-open "$BLOG_URL"
+open "http://localhost:${LOCALPORT}"
